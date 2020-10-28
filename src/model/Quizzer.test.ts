f871db4e6543063666d9423cf8c863eba8383271
Michael Tughan <mtughan@gmail.com>
2020-10-27 23:28:06 -0400

    Add support for correct answers
    No special handling is made for bonuses or error points yet.

--- src/model/Quizzer.test.ts
+++ src/model/Quizzer.test.ts
@@ -1,11 +1,58 @@
 import Quizzer from './Quizzer';
 
+const name = 'Michael Tughan';
+
 test('quizzer with name returns that name', () => {
-  const quizzer = new Quizzer('Michael Tughan');
+  const quizzer = new Quizzer(name);
   expect(quizzer.name).toBe('Michael Tughan');
 });
 
 test('quizzer with no questions answered has score 0', () => {
-  const quizzer = new Quizzer('Michael Tughan');
+  const quizzer = new Quizzer(name);
   expect(quizzer.getScore()).toBe(0);
+});
+
+test('quizzer with 1 question answered correctly has score 20', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(20);
+});
+
+test('quizzer with 2 questions answered correctly has score 40', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(40);
+});
+
+test('quizzer with 3 questions answered correctly has score 60', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(60);
+});
+
+test('quizzer with 1 question answered correctly and 1 incorrectly has score 20', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(false);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(20);
+});
+
+test('quizzer with 2 questions answered correctly and 1 incorrectly has score 40', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(false);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(40);
+});
+
+test('quizzer with 3 questions answered correctly and 1 incorrectly has score 60', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(false);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(60);
 });
--- src/model/Quizzer.ts
+++ src/model/Quizzer.ts
@@ -1,10 +1,21 @@
 class Quizzer {
-  #score = 0;
+  #results: boolean[] = [];
 
   constructor(public readonly name: string) {}
 
+  addQuestionResult(correct: boolean): void {
+    this.#results.push(correct);
+  }
+
   getScore(): number {
-    return this.#score;
+    let correct = 0;
+    this.#results.forEach((result) => {
+      if (result) {
+        correct += 1;
+      }
+    });
+
+    return correct * 20;
   }
 }
 
