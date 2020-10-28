138d272f8990c363ddc0a88d2b8babf750ba7667
Michael Tughan <mtughan@gmail.com>
2020-10-27 23:34:38 -0400

    Add support for quiz out bonuses

--- src/model/Quizzer.test.ts
+++ src/model/Quizzer.test.ts
@@ -33,6 +33,15 @@ test('quizzer with 3 questions answered correctly has score 60', () => {
   expect(quizzer.getScore()).toBe(60);
 });
 
+test('quizzer with 4 questions answered correctly has score 90', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(90);
+});
+
 test('quizzer with 1 question answered correctly and 1 incorrectly has score 20', () => {
   const quizzer = new Quizzer(name);
   quizzer.addQuestionResult(false);
@@ -55,4 +64,14 @@ test('quizzer with 3 questions answered correctly and 1 incorrectly has score 60
   quizzer.addQuestionResult(false);
   quizzer.addQuestionResult(true);
   expect(quizzer.getScore()).toBe(60);
+});
+
+test('quizzer with 4 questions answered correctly and 1 incorrectly has score 80', () => {
+  const quizzer = new Quizzer(name);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(false);
+  quizzer.addQuestionResult(true);
+  quizzer.addQuestionResult(true);
+  expect(quizzer.getScore()).toBe(80);
 });
--- src/model/Quizzer.ts
+++ src/model/Quizzer.ts
@@ -9,13 +9,20 @@ class Quizzer {
 
   getScore(): number {
     let correct = 0;
+    let incorrect = 0;
     this.#results.forEach((result) => {
       if (result) {
         correct += 1;
+      } else {
+        incorrect += 1;
       }
     });
 
-    return correct * 20;
+    let bonus = 0;
+    if (correct >= 4 && incorrect === 0) {
+      bonus = 10;
+    }
+    return correct * 20 + bonus;
   }
 }
 
