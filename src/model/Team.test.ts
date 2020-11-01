7d5cc6844eed877f6d6128c8f07a78bc446b21db
Michael Tughan <mtughan@gmail.com>
2020-11-01 00:36:19 -0400

    Allow adding quizzers to a team

--- src/model/Team.test.ts
+++ src/model/Team.test.ts
@@ -1,6 +1,30 @@
+import Quizzer from './Quizzer';
 import Team from './Team';
 
 test('score with no questions answered is 20', () => {
   const team = new Team();
   expect(team.getScore()).toBe(20);
+});
+
+test('quizzers can be added to a team', () => {
+  const team = new Team();
+  team.setQuizzer(0, new Quizzer('Quizzer 1'));
+  team.setQuizzer(3, new Quizzer('Quizzer 2'));
+
+  const quizzers = team.getQuizzers();
+  expect(quizzers).toHaveLength(5);
+  expect(quizzers[0]?.name).toBe('Quizzer 1');
+  expect(quizzers[1]).toBeNull();
+  expect(quizzers[2]).toBeNull();
+  expect(quizzers[3]?.name).toBe('Quizzer 2');
+  expect(quizzers[4]).toBeNull();
+});
+
+test('quizzers in invalid seats are rejected', () => {
+  const team = new Team();
+  const quizzer = new Quizzer('Quizzer');
+
+  expect(() => team.setQuizzer(-1, quizzer)).toThrow();
+  expect(() => team.setQuizzer(5, quizzer)).toThrow();
+  expect(() => team.setQuizzer(NaN, quizzer)).toThrow();
 });
--- src/model/Team.ts
+++ src/model/Team.ts
@@ -1,8 +1,29 @@
+import Quizzer from './Quizzer';
+
 class Team {
+  private static readonly MAX_QUIZZERS = 5;
+
   #score: number;
+
+  #quizzers: Map<number, Quizzer> = new Map<number, Quizzer>();
 
   constructor() {
     this.#score = 20;
+  }
+
+  setQuizzer(index: number, quizzer: Quizzer): void {
+    if (index < 0 || index >= Team.MAX_QUIZZERS || Number.isNaN(index)) { // max 5 quizzers
+      throw new Error('index must be between 0 and 4!');
+    }
+    this.#quizzers.set(index, quizzer);
+  }
+
+  getQuizzers(): (Quizzer | null)[] {
+    const quizzers: (Quizzer | null)[] = [];
+    for (let i = 0; i < Team.MAX_QUIZZERS; i += 1) {
+      quizzers.push(this.#quizzers.get(i) ?? null);
+    }
+    return quizzers;
   }
 
   getScore(): number {
